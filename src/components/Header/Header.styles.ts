5534d25cc19ef58539f53cff6bdfdfe938bc393c
james15394 <70645448+james15394@users.noreply.github.com>
2021-05-27 21:10:27 +0700

    Update Header.styles.ts

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -26,7 +26,9 @@ export const HeaderLeft = styled.div<Prop>`
   display: flex;
   justify-content: center;
   align-items: center;
-
+.search {
+      position: relative; 
+}
   .item {
     padding-right: 30px;
     color: ${(props) => (props.navScroll ? "#1E6872" : "#fff")};
@@ -47,10 +49,22 @@ export const HeaderLeft = styled.div<Prop>`
       width: ${(props) => (props.openSearch ? "250px" : "40px")};
       height: 40px;
       border-radius: ${(props) => (props.openSearch ? "30px" : "40px")};
+      transition: all 0.5s;
       transition: 0.5s;
       overflow: hidden;
       background: ${(props) => (props.navScroll ? "#289FAC" : "#fff")};
       box-shadow:  5px 5px 5px rgba(0,0,0,0.2);
+             input:-webkit-autofill,
+        input:-webkit-autofill:hover,
+        input:-webkit-autofill:focus,
+        input:-webkit-autofill:active {
+        border: none;
+        -webkit-text-fill-color: ${(props) =>
+          props.navScroll ? "#fff" : "#000"};
+        -webkit-box-shadow: 0 0 0 1000px ${(props) =>
+          props.navScroll ? "#289FAC" : "#fff"} inset !important;
+        transition: all 0.5s ;
+      }
       i {
         cursor: pointer;
         position: absolute;
@@ -62,17 +76,20 @@ export const HeaderLeft = styled.div<Prop>`
         background: ${(props) => (props.navScroll ? "#289FAC" : "#fff")};
         color: ${(props) => (props.navScroll ? "#fff" : "#289FAC")};
         display: flex;
+        transition: all 0.5s;
         justify-content: center;
         transition: 0.5s;
         align-items: center;
       }
-      input[type='text'] {
-        position: relative;
-        left: 40px;
+      input {
+        margin-left: 40px;
+        top: 0;
+        bottom: 0;
         width: 100%;
         border: none;
         outline-width: 0;
         transition: all 0.5s;
+        color: ${(props) => (props.navScroll ? "#fff" : "#000")}
         background: ${(props) => (props.navScroll ? "#289FAC" : "#fff")};
         ::placeholder {
           color: ${(props) => (!props.navScroll ? "#289FAC" : "#fff")};
